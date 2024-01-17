f071aafe3b52232c933c85dd0ce7e0c47b32b16a
AdamRadziak <adam.radziak@gmail.com>
2024-01-17 20:03:00 +0100

    @FIX - withdraw with odd nominals in atm

--- src/java/Bankomat.java
+++ src/java/Bankomat.java
@@ -1,5 +1,7 @@
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
@@ -10,142 +12,94 @@ import java.util.ArrayList;
  * @author adamr
  */
 public class Bankomat {
-
-    public static ArrayList<BankomatNominal> Kasa = new ArrayList<>();
+    // list of nominals and count of nominals in atm
+    public static ArrayList<BankomatNominal> Checkout = new ArrayList<>();
+    // value of all nominals in atm
     public static int Sum_of_nominals;
-    
-    public Bankomat() {
 
-    }
-
-    public static void sort_nominals_desc_in_kasa() {
-        /* sorting from highest to lowest*/
-        for (int j = 0; j < 30; j++) {
-
-            boolean is_change = false;
-
-            for (int i = 0; i < Kasa.size() - 1; i++) {
-
-                BankomatNominal actual = Kasa.get(i);
-                BankomatNominal next = Kasa.get(i + 1);
-
-                if (next.getNominal() > actual.getNominal()) {
-                    is_change = true;
-                    Kasa.set(i, next);
-                    Kasa.set(i + 1, actual);
-
-                }
-                if (is_change = false) {
-                    break;
-                }
-            }
-        }
-    }
-    
-       public static ArrayList<Integer> sort_desc(ArrayList<Integer> list) {
-        /* sorting from highest to lowest*/
-        // create temp list to add everything from list
-        ArrayList<Integer> sorted_list = new ArrayList<>();
-        for(int el:list){
-            sorted_list.add(el);
-        }
-        // next sort sorted list
-        for (int j = 0; j < 30; j++) {
-
-            boolean is_change = false;
-
-            for (int i = 0; i < sorted_list.size() - 1; i++) {
-
-                int actual = sorted_list.get(i);
-                int next = sorted_list.get(i + 1);
-
-                if (next > actual) {
-                    is_change = true;
-                    sorted_list.set(i, next);
-                    sorted_list.set(i + 1, actual);
-
-                }
-                if (is_change = false) {
-                    break;
-                }
-            }
-        }
-        return sorted_list;
-    }
 
     public String withdraw(int amount) {
+        // withdrawed nominals
         ArrayList<Integer> nominals_withdraw = new ArrayList<>();
+        // withdrawed nominals count
         ArrayList<Integer> nominals_counts_withdraw = new ArrayList<>();
+        // this comparator is to sort reverse order in nominals
+        Comparator<BankomatNominal> compareByNominaldesc = Comparator
+                                                            .comparing(BankomatNominal::getNominal)
+                                                            .reversed();
         String message = "Wypłacono: \n";
         // initialize remain_amount to amount
         int remain_amount = amount;
+        // nominla in chekout
         int nominal;
+        // max nominla count inn checkout
         int nominal_count;
+        // sum of withhdrawed nominals
         int withdraw_sum = 0;
-        // sort nominlas from highest to lowest
-        sort_nominals_desc_in_kasa();
-        for (int i = 0; i < Kasa.size(); i++) {
-            nominal = Kasa.get(i).getNominal();
-            nominal_count = Kasa.get(i).getCount();
-            // add to arraylist nominals
-            nominals_withdraw.add(nominal);
-            nominals_counts_withdraw.add(remain_amount / nominal);
-            // if nominals withdraw count more than nominals in bankomat
-            if (nominals_counts_withdraw.get(i) > nominal_count) {
-                nominals_counts_withdraw.set(i, nominal_count);
-            }
-            remain_amount -= nominal * nominals_counts_withdraw.get(i);
-            // refresh nominal count in cash mashine
-//            Kasa.get(i).subNominalCount(nominal, nominals_counts_withdraw.get(i));
-            
-        }
-    if (remain_amount!= 0) {
-        message = "Nie można wypłacić żądanej kwoty\n";
-        message += "Żądana kwota " + amount;
-        message += " Dostępne nominały\n";
-        for(int i = 0; i < Kasa.size(); i++){
-            message += Kasa.get(i).getNominal() + "x" + Kasa.get(i).getCount() + " ";
-        }
-    }
-
-    else {
-    for (int i = 0; i < nominals_withdraw.size(); i++) {
-        withdraw_sum += nominals_withdraw.get(i)*nominals_counts_withdraw.get(i);
-        // refresh nominal count in cash mashine
-        Kasa.get(i).subNominalCount(nominals_withdraw.get(i), nominals_counts_withdraw.get(i));
-        message = message + " " + nominals_withdraw.get(i) + " x " + nominals_counts_withdraw.get(i) + "\n";
-    }
-        Sum_of_nominals -= withdraw_sum;
-        message = message + "Wypłacona kwota: " + withdraw_sum + "\n";
-        message = message + "Pozostało środków: " + Sum_of_nominals;
-        
-    }
-    return message ;
-}
-
-public String payment(ArrayList<Integer> nominal,ArrayList<Integer> amount){
-    String message = "Wpłacono \n";
-    int payment_sum = 0;
-    ArrayList<Integer> nominals_sort_desc = new ArrayList<>();
-    ArrayList<Integer> amount_sort_desc = new ArrayList<>();
-    // first sorting nominals arraylist desc
-    nominals_sort_desc = sort_desc(nominal);
-    amount_sort_desc = sort_desc(amount);
-        for(int i =0; i < nominals_sort_desc.size(); i++){
-            // check if nominal is in kasa and Kasa is not empty
-            if ( i < Kasa.size() && Kasa.get(i).getNominal() == nominals_sort_desc.get(i)){
-                Kasa.get(i).addNominalCount(nominals_sort_desc.get(i), amount_sort_desc.get(i));
+        // count of n=withdrawed nominals
+        int withdraw_nominal_count = 0;
+        // sort nominals from highest to lowest by Nominal
+        Checkout.sort(compareByNominaldesc);
+        for (int i = 0; i < Checkout.size(); i++) {
+            nominal = Checkout.get(i).getNominal();
+            nominal_count = Checkout.get(i).getCount();
+            // if  remain_amount is nominal * nominal count in checkout
+            if(remain_amount % nominal == 0){
+                withdraw_nominal_count = remain_amount/nominal;
             }
             else{
-                Kasa.add(new BankomatNominal(nominals_sort_desc.get(i), amount_sort_desc.get(i)));
+                withdraw_nominal_count = (remain_amount -(remain_amount % nominal))/nominal ;
             }
-            payment_sum += nominals_sort_desc.get(i)*amount_sort_desc.get(i);
-        message = message + " " + nominals_sort_desc.get(i) + " x " + amount_sort_desc.get(i);
+            // if nominals withdraw count more than nominals in checkout
+            if(withdraw_nominal_count >= nominal_count){
+                withdraw_nominal_count = nominal_count;
+            }
+            // add to arraylist nominals
+            nominals_withdraw.add(nominal);
+            nominals_counts_withdraw.add(withdraw_nominal_count);
+            // equal remain account
+            remain_amount -= nominal * nominals_counts_withdraw.get(i);
+        }
+        if (remain_amount != 0) {
+            message = "Nie można wypłacić żądanej kwoty\n";
+            message += "Żądana kwota " + amount;
+            message += " Dostępne nominały\n";
+            for (int i = 0; i < Checkout.size(); i++) {
+                message += Checkout.get(i).getNominal() + "x" + Checkout.get(i).getCount() + " ";
+            }
+        } else {
+            for (int i = 0; i < nominals_withdraw.size(); i++) {
+                withdraw_sum += nominals_withdraw.get(i) * nominals_counts_withdraw.get(i);
+                // refresh nominal count in cash mashine
+                Checkout.get(i).subNominalCount(nominals_withdraw.get(i), nominals_counts_withdraw.get(i));
+                message = message + " " + nominals_withdraw.get(i) + " x " + nominals_counts_withdraw.get(i) + "\n";
+            }
+            Sum_of_nominals -= withdraw_sum;
+            message = message + "Wypłacona kwota: " + withdraw_sum + "\n";
+            message = message + "Pozostało środków: " + Sum_of_nominals;
+            // sorting in ascending order nominals
+            Checkout.sort(Comparator.comparing(BankomatNominal::getNominal));
+        }
+        return message;
     }
+
+    public String payment(ArrayList<Integer> nominal, ArrayList<Integer> amount) {
+        String message = "Wpłacono \n";
+        int payment_sum = 0;
+        // first sorting nominals arraylist desc
+        for (int i = 0; i < nominal.size(); i++) {
+            // check if nominal is in kasa and Checkout is not empty
+            if (i < Checkout.size() && Checkout.get(i).getNominal() == nominal.get(i)) {
+                Checkout.get(i).addNominalCount(nominal.get(i), amount.get(i));
+            } else {
+                Checkout.add(new BankomatNominal(nominal.get(i), amount.get(i)));
+            }
+            payment_sum += nominal.get(i) * amount.get(i);
+            message = message + " " + nominal.get(i) + " x " + amount.get(i);
+        }
         Sum_of_nominals += payment_sum;
         message = message + "\n" + "Suma wpłat:\n" + payment_sum;
         message = message + "\n" + "Stan konta po wpłacie:\n" + Sum_of_nominals;
-    return message;
+        return message;
+    }
 }
-}
-
